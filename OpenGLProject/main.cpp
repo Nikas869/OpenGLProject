0511e9f66c243c6aba652d03c98757308d64e789
Nikita Magda <nikmagdaa@gmail.com>
2018-04-08 17:09:06 +0300

    Added fps counter and green screen

--- OpenGLProject/main.cpp
+++ OpenGLProject/main.cpp
@@ -1,18 +1,11 @@
 #include <iostream>
-#include "wtypes.h"
 // GLEW
 #define GLEW_STATIC
 #include <GL/glew.h>
 // GLFW
 #include <GLFW/glfw3.h>
 
-void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
-{
-    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-    {
-        glfwSetWindowShouldClose(window, GL_TRUE);
-    }
-}
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
 
 int main()
 {
@@ -24,7 +17,7 @@ int main()
 
     GLFWmonitor* primaryMonitor = glfwGetPrimaryMonitor();
     const GLFWvidmode* videoMode = glfwGetVideoMode(primaryMonitor);
-    GLFWwindow* window = glfwCreateWindow(videoMode->width, videoMode->height, "OpenGL Project", primaryMonitor, nullptr);
+    GLFWwindow* window = glfwCreateWindow(videoMode->width, videoMode->height, "OpenGL Project", nullptr, nullptr);
     if (window == nullptr)
     {
         std::cout << "Failed to create window" << std::endl;
@@ -45,12 +38,36 @@ int main()
     glfwGetFramebufferSize(window, &width, &height);
     glViewport(0, 0, width, height);
 
+    double lastTime = glfwGetTime();
+    int nbFrames = 0;
+
     while (!glfwWindowShouldClose(window))
     {
+        // fps counter
+        double currentTime = glfwGetTime();
+        nbFrames++;
+        if (currentTime - lastTime >= 1.0) {
+            printf("%f ms/frame, %i fps\n", 1000.0 / double(nbFrames), nbFrames);
+            nbFrames = 0;
+            lastTime += 1.0;
+        }
+
         glfwPollEvents();
+
+        glClearColor(0.2f, 0.4f, 0.1f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
         glfwSwapBuffers(window);
     }
 
     glfwTerminate();
     return 0;
+}
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+    {
+        glfwSetWindowShouldClose(window, GL_TRUE);
+    }
 }
