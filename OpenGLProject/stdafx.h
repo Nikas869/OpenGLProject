5860203ad6f783e21e9dda0acb6208604d250c7e
Nikita Magda <nikmagdaa@gmail.com>
2018-04-20 01:21:49 +0300

    Added GLM

--- OpenGLProject/stdafx.h
+++ OpenGLProject/stdafx.h
@@ -5,9 +5,15 @@
 #include <iostream>
 #include <string>
 #include <chrono>
+
 #include <gl\GLU.h>
 #include "Include\GL\glext.h"
 #include "Include\GL\wglext.h"
+
+#include "Include\glm\glm.hpp"
+#include "Include\glm\gtc\matrix_transform.hpp"
+#include "Include\glm\gtc\type_ptr.hpp"
+
 #include "window.h"
 #include "openglwrapper.h"
 #include "renderer.h"
